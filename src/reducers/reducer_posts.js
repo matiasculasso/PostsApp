0118140f13e28f9d089746bc7f623a82f3001ad4
Matias Culasso Paulin <matiasculasso@gmail.com>
2018-02-17 14:50:58 -0300

    test 2

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,8 +5,11 @@ export default function (state = {}, action)  {
     switch (action.type)
     {
         case DELETE_POST:
-            
-        case FETCH_POSTS:             
+            // this method removes the property of the state object if exists
+            // in the delete method the payload is the id of the deleted post 
+            _.omit(state, action.payload)
+        case FETCH_POSTS:   
+            // maps an array to an object using the id in this case as the property name          
             return _.mapKeys(action.payload.data, 'id');
         case FETCH_POST:             
             // this ES6 sintax adds to the state object a new properity
